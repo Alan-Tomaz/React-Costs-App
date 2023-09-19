6a6bf2b80f7bcdda5ce687777c8c832a2c5e27b1
Alan <alan4tomaz8@gmail.com>
2023-09-18 22:26:53 -0300

    Second Commit - 18/09/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import NewProject from './components/pages/NewProject';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import Home from './components/pages/Home';
+import Container from './components/layout/Container';
 
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+      <Container customClass='min_height'>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/company' element={<Company/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/newproject' element={<NewProject/>}/>
+      </Routes>
+    </Container>
+      <p>Footer</p>
+    </Router>
   );
 }
 
