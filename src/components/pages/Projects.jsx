5d8c872f9cc4d57c42c834d78d7dc0a83d0f7aca
Alan <alan4tomaz8@gmail.com>
2023-09-26 23:17:34 -0300

    Ninth Commit - 26/09/2023

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,8 +3,26 @@ import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 import Container from '../layout/Container';
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
+import { useEffect, useState } from 'react';
 
 function Projects() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                setProjects(data);
+            })
+            .catch(err => console.log(err))
+    }, [])
 
     const location = useLocation();
     let message = '';
@@ -20,7 +38,19 @@ function Projects() {
             </div>
             {message && <Message type="success" msg={message} />}
             <Container customClass="start">
-                <p>Projects...</p>
+                {projects.length > 0 &&
+                    projects.map(project =>
+                    (
+                        <ProjectCard
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    )
+                    )
+                }
             </Container>
         </div>
     )
